265f76fa9e7f84a37d019f3bca9a90bd113315c9
Winfred <winfrednginakilonzo@gmail.com>
2023-12-09 12:32:41 +0300

    Lockboxes

---
+++ 0x01-lockboxes/0-lockboxes.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""Determines if all the boxes can be opened"""
+
+
+def canUnlockAll(boxes):
+    """ Method that determines if all boxes can be opened """
+
+    for key in range(1, len(boxes)):
+        flag = False
+        for box in range(len(boxes)):
+            if key in boxes[box] and box != key:
+                flag = True
+                break
+        if not flag:
+            return False
+
+    return True

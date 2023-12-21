2225e9a6eec763e99392e2b45ff8c8c7e1574635
Winfred <winfrednginakilonzo@gmail.com>
2023-12-21 12:30:38 +0300

    Raise excemption again

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -1,52 +1,49 @@
 #!/usr/bin/python3
 """
-script that reads stdin line by line and computes metrics
+Task: 0. Log Parsing
+File: 0x06-log_parsing/0-stats.py
 """
-import sys
-import signal
-
-# Initialize variables
-status_codes = [200, 301, 400, 401, 403, 404, 405, 500]
-status_dict = dict.fromkeys(status_codes, 0)
-total_size = 0
-line_count = 0
+from sys import stdin
 
 
-# Function to print statistics
-def print_stats():
-    print("File size: {}".format(total_size))
-    for code in sorted(status_dict):
-        if status_dict[code] > 0:
-            print("{}: {}".format(code, status_dict[code]))
+def printstats(file_size, status_codes):
+    """
+    This prints statistics at the beginning and every 10 lines
+    This will also be called on a Keyboard interruption
+    """
+    print("File size: " + str(file_size))
+    for code in sorted(status_codes.keys()):
+        if status_codes[code] > 0:
+            print(code + ": " + str(status_codes[code]))
 
 
-# Function to handle keyboard interruption
-def signal_handler(sig, frame):
-    print_stats()
-    sys.exit(0)
-
-
-# Set signal handler
-signal.signal(signal.SIGINT, signal_handler)
+line_num = 0
+file_size = 0
+status_code = 0
+status_codes = {"200": 0, "301": 0, "400": 0, "401": 0,
+                "403": 0, "404": 0, "405": 0, "500": 0}
 
 try:
-    for line in sys.stdin:
-        try:
-            parts = line.split()
-            size = int(parts[-1])
-            status = int(parts[-2])
-            total_size += size
-            if status in status_codes:
-                status_dict[status] += 1
-        except (IndexError, ValueError):
-            continue
+    for line in stdin:
+        line_num += 1
+        split_line = line.split()
 
-        line_count += 1
-        if line_count % 10 == 0:
-            print_stats()
+        if len(split_line) > 1:
+            file_size += int(split_line[-1])
 
-except KeyboardInterrupt:
-    pass
+        if len(split_line) > 2 and split_line[-2].isnumeric():
+            status_code = split_line[-2]
+        else:
+            status_code = 0
 
-finally:
-    print_stats()
+        if status_code in status_codes.keys():
+            status_codes[status_code] += 1
+
+        if line_num % 10 == 0:
+            printstats(file_size, status_codes)
+
+    printstats(file_size, status_codes)
+
+except (KeyboardInterrupt):
+    printstats(file_size, status_codes)
+    raise

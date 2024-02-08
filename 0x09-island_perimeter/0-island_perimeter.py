ac198377274c9bdffbdd264815c00d8589ae8ae6
Winfred <winfrednginakilonzo@gmail.com>
2024-02-08 18:37:07 +0300

    Fixed code based on edge cases

--- 0x09-island_perimeter/0-island_perimeter.py
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -10,11 +10,23 @@ def island_perimeter(grid):
     Cells are connected horizontally/vertically (not diagonally).
     """
     perimeter = 0
-    index = 0
     for i in range(len(grid)):
-        for j in range(len(grid[1])-1, -1, -1):
+        for j in range(len(grid[0])):
+            # Check if the current cell contains land (has a value of 1)
             if grid[i][j] == 1:
-                index = (j + 1) * 2
-        perimeter += index
-        index = 0
+                # Add 4 to the perimeter for each land cell
+                perimeter += 4
+
+                # Check if the cell above the current cell is also land
+                if i > 0 and grid[i - 1][j] == 1:
+                    """Subtract 2 from the perimeter for
+                    shared edge with the cell above"""
+                    perimeter -= 2
+
+                """Check if the cell to the left of the
+                current cell is also land"""
+                if j > 0 and grid[i][j - 1] == 1:
+                    """Subtract 2 from the perimeter for
+                    shared edge with the cell to the left"""
+                    perimeter -= 2
     return perimeter
